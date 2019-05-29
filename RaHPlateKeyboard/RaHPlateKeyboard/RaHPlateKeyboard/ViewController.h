e038421e2798096214f1797c2f7d46f828be20d0
ROOSTERLIN <rooster2578@gmail.com>
2019-05-29 22:53:02 +0800

    merge

---
+++ RaHPlateKeyboard/RaHPlateKeyboard/RaHPlateKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RaHPlateKeyboard
+//
+//  Created by ROOSTERLIN on 2019/5/29.
+//  Copyright © 2019 ROOSTERLIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RaHPlateKeyboard/RaHPlateKeyboard/RaHPlateKeyboard/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RaHPlateKeyboard
+//
+//  Created by ROOSTERLIN on 2019/5/29.
+//  Copyright © 2019 ROOSTERLIN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
